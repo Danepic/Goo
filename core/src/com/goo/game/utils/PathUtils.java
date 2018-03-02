afe6640404a60b011da8b200d3583b1e4b81a08d
Danilo Mendes <danilo.silva@seniorsolution.com.br>
2018-03-02 14:18:38 -0300

    Correções nos botões melhorias

---
+++ core/src/com/goo/game/utils/PathUtils.java
@@ -0,0 +1,51 @@
+package com.goo.game.utils;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+/**
+ * Created by danilo.silva on 3/1/2018.
+ */
+
+public class PathUtils {
+
+    public static ImageButton textureRegion(String path, int posx, int posy){
+
+        Texture button = new Texture(path);
+        TextureRegion buttonRegion = new TextureRegion(button);
+        TextureRegionDrawable buttonDrawable = new TextureRegionDrawable(buttonRegion);
+
+        ImageButton img = new ImageButton(buttonDrawable);
+        img.setPosition(posx - button.getWidth()/2, posy - button.getHeight()/2);
+
+        return img;
+
+    }
+
+    public static Image texture(String path, int posx, int posy, float scaleX, float scaleY){
+
+        Texture imgTexture = new Texture(path);
+        TextureRegion imgRegion = new TextureRegion(imgTexture);
+        TextureRegionDrawable imgDrawable = new TextureRegionDrawable(imgRegion);
+
+        Image img = new Image(imgDrawable);
+        img.setPosition(posx - imgTexture.getWidth()/2, posy - imgTexture.getHeight()/2);
+        img.setScale(scaleX, scaleY);
+
+        return img;
+
+    }
+
+    public static Sprite sprite(String path, int posx, int posy, float scale){
+
+        Sprite sprite = new Sprite(new Texture(path));
+        sprite.setPosition(posx, posy);
+        sprite.setScale(scale);
+        return sprite;
+
+    }
+}
--- core/src/com/goo/game/view/MainMenuScreen.java
+++ core/src/com/goo/game/view/MainMenuScreen.java
@@ -5,13 +5,16 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.goo.game.utils.PathUtils;
+import com.goo.game.view.world1.AdventureTimeScreen;
 
 /**
  * Created by TIO BIGA on 12/02/2018.
@@ -21,9 +24,19 @@ public class MainMenuScreen implements Screen {
 
     private Game game;
 
+    private SpriteBatch batch;
+
     private Stage stage;
 
+    private Image logo;
+    private float rotateLogo;
+
     private ImageButton playButton;
+    private ImageButton playOption;
+    private ImageButton playExit;
+    private ImageButton menu;
+    private ImageButton menuDireito;
+    private ImageButton menuEsquerdo;
 
     public MainMenuScreen(Game game){
         this.game = game;
@@ -31,14 +44,68 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void show() {
-        Texture play = new Texture("components/play.png");
-        TextureRegion playRegion = new TextureRegion(play);
-        TextureRegionDrawable playDrawable = new TextureRegionDrawable(playRegion);
-        playButton = new ImageButton(playDrawable);
 
+        //Batch
+        batch = new SpriteBatch();
+
+        //Var Utils
+        int posFinalX = Gdx.graphics.getWidth();
+        int posFinalY = Gdx.graphics.getWidth();
+
+        int meioTelaX = Gdx.graphics.getWidth()/2;
+        int meioTelaY = Gdx.graphics.getHeight()/2;
+
+        //Sprite
+        logo = PathUtils.texture("components/logo.png", 185, meioTelaY + 200, 1.70f, 1.70f);
+
+        //Buttons
+        playButton = PathUtils.textureRegion("components/play.png", meioTelaX, meioTelaY);
+        playOption = PathUtils.textureRegion("components/option.png", meioTelaX - 45, 75);
+        playExit = PathUtils.textureRegion("components/exit.png", meioTelaX + 45, 75);
+
+        //Components
+        menu = PathUtils.textureRegion("components/menu000.png", 0, 0);
+        menuDireito = PathUtils.textureRegion("components/menu100.png", 0, 0);
+        menuEsquerdo = PathUtils.textureRegion("components/menu200.png", 0, 0);
+
+        //Stage
         stage = new Stage(new ScreenViewport()); //Set up a stage for the ui
         stage.addActor(playButton); //Add the button to the stage to perform rendering and take input.
+        stage.addActor(playOption); //Add the button to the stage to perform rendering and take input.
+        stage.addActor(playExit); //Add the button to the stage to perform rendering and take input.
+        stage.addActor(logo);
+        stage.addActor(menu);
+        stage.addActor(menuDireito);
+        stage.addActor(menuEsquerdo);
         Gdx.input.setInputProcessor(stage); //Start taking input from the ui
+
+        //Eventos
+        playButton.addListener(new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                Gdx.app.log("Ação","Botão 'Play' tocado...");
+                game.setScreen(new AdventureTimeScreen(game));
+                return false;
+            }
+        });
+
+        playOption.addListener(new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                Gdx.app.log("Ação","Botão 'Play' tocado...");
+                game.setScreen(new AdventureTimeScreen(game));
+                return false;
+            }
+        });
+
+        playExit.addListener(new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                Gdx.app.log("Ação","Saindo do jogo...");
+                Gdx.app.exit();
+                return false;
+            }
+        });
     }
 
     @Override
@@ -46,16 +113,19 @@ public class MainMenuScreen implements Screen {
         Gdx.gl.glClearColor(1,1,1,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        playButton.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                game.setScreen(new MenuScreen(game));
-            }
-        });
+        batch.begin();
 
+        rotateLogo = 100 * delta;
+
+        stage.getActors().get(3).setOrigin(logo.getImageWidth()/2, logo.getImageHeight()/2);
+        stage.getActors().get(3).rotateBy(rotateLogo);
         stage.act(Gdx.graphics.getDeltaTime()); //Perform ui logic
         stage.draw(); //Draw the ui
+
+        batch.end();
     }
+
+
 
     @Override
     public void resize(int width, int height) {
